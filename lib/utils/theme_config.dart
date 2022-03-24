1a38e554e385220ae94c3e60c844c6fd75ffa58e
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-03-24 19:32:02 +0530

    added themes and first page half done

---
+++ lib/utils/theme_config.dart
@@ -0,0 +1,230 @@
+// import 'package:insync/utils/Constants.dart';
+import 'package:flutter/material.dart';
+
+// // dark mode switch
+// enum MyTheme { light, dark }
+// String? _darkmode = Constants.prefs.getString("darkmode");
+
+// class ThemeNotifier extends ChangeNotifier {
+//   static List<ThemeData> themes = [
+//     //light theme
+//     ThemeData(
+//       dividerColor: Colors.transparent,
+//       visualDensity: VisualDensity.adaptivePlatformDensity,
+//       cardTheme: const CardTheme(
+//         shadowColor: Color(0x1100d2ff),
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(20)),
+//         ),
+//         elevation: 10,
+//       ),
+//       inputDecorationTheme: const InputDecorationTheme(
+//         hintStyle: TextStyle(
+//           color: Color(0xffCBC6CB),
+//         ),
+//         filled: true,
+//         fillColor: Colors.white,
+//         // fillColor: Color(0xffF3F0F4),
+//         hoverColor: Colors.white,
+//         alignLabelWithHint: true,
+//         border: InputBorder.none,
+//         enabledBorder: OutlineInputBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(50.0)),
+//           borderSide: BorderSide(color: Color(0xffF3F0F4)),
+//         ),
+//         focusedBorder: OutlineInputBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(50.0)),
+//           borderSide: BorderSide(color: Color(0xffF3F0F4)),
+//         ),
+//       ),
+//       bottomAppBarTheme: const BottomAppBarTheme(
+//         color: Colors.white,
+//         elevation: 0,
+//       ),
+//       appBarTheme: const AppBarTheme(
+//         color: Color(0xffF7F7FF),
+//         elevation: 0,
+//         centerTitle: true,
+//         iconTheme: IconThemeData(
+//           color: Colors.black,
+//         ),
+//       ),
+//       primaryColor: const Color(0xff2dadc2),
+//       primaryColorLight: const Color(0xff00d2ff),
+//       primaryColorDark: const Color(0xff0052ff),
+//       scaffoldBackgroundColor: const Color(0xffF7FAFF),
+//       fontFamily: 'GeneralSans',
+//       brightness: Brightness.light,
+//       backgroundColor: const Color(0xff393E46),
+//       floatingActionButtonTheme: const FloatingActionButtonThemeData(
+//         elevation: 0,
+//         backgroundColor: Color(0xff2dadc2),
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(20)),
+//         ),
+//         foregroundColor: Colors.white,
+//       ),
+//       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+//         backgroundColor: Colors.white,
+//         elevation: 0,
+//         selectedItemColor: Color(0xff2dadc2),
+//       ),
+//     ),
+//     //dark theme
+//     ThemeData(
+//       dividerColor: Colors.transparent,
+//       visualDensity: VisualDensity.adaptivePlatformDensity,
+//       cardTheme: const CardTheme(
+//         color: Color(0xff1d1d1d),
+//         shadowColor: Color(0x00333333),
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(20)),
+//         ),
+//         elevation: 0,
+//       ),
+//       floatingActionButtonTheme: const FloatingActionButtonThemeData(
+//         elevation: 4,
+//         backgroundColor: Color(0xff2dadc2),
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(20)),
+//         ),
+//         foregroundColor: Colors.white,
+//       ),
+//       appBarTheme: const AppBarTheme(
+//         color: Colors.black45,
+//         elevation: 0,
+//         centerTitle: true,
+//         iconTheme: IconThemeData(
+//           color: Colors.white,
+//         ),
+//         actionsIconTheme: IconThemeData(
+//           color: Colors.white,
+//         ),
+//       ),
+//       inputDecorationTheme: const InputDecorationTheme(
+//         hintStyle: TextStyle(
+//           color: Color(0xff555555),
+//         ),
+//         filled: true,
+//         fillColor: Color(0xff272727),
+//         hoverColor: Colors.white,
+//         alignLabelWithHint: true,
+//         border: InputBorder.none,
+//         enabledBorder: OutlineInputBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(50.0)),
+//           borderSide: BorderSide(color: Color(0x00000000)),
+//         ),
+//         focusedBorder: OutlineInputBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(50.0)),
+//           borderSide: BorderSide(color: Color(0x00000000)),
+//         ),
+//       ),
+//       primaryColor: const Color(0xff2dadc2),
+//       primaryColorLight: const Color(0xff00d2ff),
+//       primaryColorDark: const Color(0xff0052ff),
+//       scaffoldBackgroundColor: const Color(0xff040505),
+//       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+//         backgroundColor: Colors.black45,
+//         elevation: 0,
+//         selectedItemColor: Color(0xff2dadc2),
+//       ),
+//       fontFamily: 'GeneralSans',
+//       brightness: Brightness.dark,
+//       backgroundColor: const Color(0xffffffff),
+//     ),
+//   ];
+
+//   MyTheme _current = (_darkmode == "false" || _darkmode == null)
+//       ? MyTheme.light
+//       : MyTheme.dark;
+//   // MyTheme _current = MyTheme.Light;
+//   ThemeData _currentTheme =
+//       (_darkmode == "false" || _darkmode == null) ? themes[0] : themes[1];
+//   // MyTheme _current = MyTheme.Dark;
+//   // ThemeData _currentTheme = themes[1];
+
+//   set currentTheme(theme) {
+//     if (theme != null) {
+//       _current = theme;
+//       _currentTheme = _current == MyTheme.light ? themes[0] : themes[1];
+//       notifyListeners();
+//     }
+//   }
+
+//   get myTheme => _current;
+
+//   ThemeData get currentTheme => _currentTheme;
+
+//   void switchTheme() {
+//     if (_current == MyTheme.light) {
+//       Constants.prefs.setString("darkmode", "true");
+//       currentTheme = MyTheme.dark;
+//     } else {
+//       Constants.prefs.setString("darkmode", "false");
+//       currentTheme = MyTheme.light;
+//     }
+//   }
+// }
+
+//light theme
+ThemeData lighttheme = ThemeData(
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+  cardTheme: const CardTheme(
+    shadowColor: Color(0x1100d2ff),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+    ),
+    elevation: 10,
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    hintStyle: TextStyle(
+      color: Color(0xffCBC6CB),
+    ),
+    filled: true,
+    fillColor: Colors.white,
+    // fillColor: Color(0xffF3F0F4),
+    hoverColor: Colors.white,
+    alignLabelWithHint: true,
+    border: InputBorder.none,
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+      borderSide: BorderSide(color: Color(0xffF3F0F4)),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+      borderSide: BorderSide(color: Color(0xffF3F0F4)),
+    ),
+  ),
+  bottomAppBarTheme: const BottomAppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+  ),
+  appBarTheme: const AppBarTheme(
+    color: Color(0xffF7F7FF),
+    elevation: 0,
+    centerTitle: true,
+    iconTheme: IconThemeData(
+      color: Colors.black,
+    ),
+  ),
+  primaryColor: const Color(0xffFD4C62),
+  primaryColorLight: const Color(0xff00d2ff),
+  primaryColorDark: const Color(0xff0052ff),
+  scaffoldBackgroundColor: const Color(0xffF7FAFF),
+  fontFamily: 'General Sans',
+  brightness: Brightness.light,
+  backgroundColor: const Color(0xff393E46),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    elevation: 0,
+    backgroundColor: Color(0xff2dadc2),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+    ),
+    foregroundColor: Colors.white,
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    selectedItemColor: Color(0xff2dadc2),
+  ),
+);
---
+++ lib/widgets/button.dart
@@ -0,0 +1,43 @@
+// import 'package:flutter/material.dart';
+
+// class Button extends StatefulWidget {
+//   const Button({
+//     @required this.myText,
+//     @required this.myColor,
+//     this.routeName,
+//     this.onPressed,
+//     Key key,
+//   }) : super(key: key);
+//   final String myText;
+//   final Color myColor;
+//   final String routeName;
+//   final Function onPressed;
+
+//   @override
+//   _ButtonState createState() => _ButtonState();
+// }
+
+// class _ButtonState extends State<Button> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return RaisedButton(
+//       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+//       textColor: Colors.white,
+//       elevation: 0,
+//       color: widget.myColor,
+//       child: Text(
+//         widget.myText,
+//         style: TextStyle(
+//             color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
+//       ),
+//       onPressed: widget.onPressed ??
+//           () {
+//             print("going to " + widget.routeName);
+//             Navigator.pushNamed(context, widget.routeName);
+//           },
+//       shape: new RoundedRectangleBorder(
+//         borderRadius: new BorderRadius.circular(16.0),
+//       ),
+//     );
+//   }
+// }
