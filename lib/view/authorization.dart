7d2bdd89cf64cf7ebc2f8a094be83402ae008361
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-04-03 23:00:15 +0530

    completed sign up form

--- lib/view/authorization.dart
+++ lib/view/authorization.dart
@@ -46,42 +46,30 @@ class _AuthorizationState extends State<Authorization> {
                   PrimaryButton(
                     buttonTitle: "create an account",
                     onPressed: () {
-                      // showModalBottomSheet<void>(
-                      //   context: context,
-                      //   builder: (BuildContext context) {
-                      //     return const CreateAccountOverlay();
-                      //   },
-                      //   shape: const RoundedRectangleBorder(
-                      //     borderRadius: BorderRadius.only(
-                      //         topLeft: Radius.circular(24),
-                      //         topRight: Radius.circular(24)),
-                      //   ),
-                      // );
+                      showModalBottomSheet<void>(
+                        context: context,
+                        isScrollControlled: true,
+                        backgroundColor: Colors.transparent,
+                        builder: (BuildContext context) {
+                          return const CreateAccountOverlay();
+                        },
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(24),
+                              topRight: Radius.circular(24)),
+                        ),
+                      );
                     },
                   ),
                   const SizedBox(height: 20),
-                  TextButton(
-                    onPressed: null,
-                    child: const Text(
-                      "already a member?",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xff1A1A1A)),
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry?>(
-                          const EdgeInsets.symmetric(
-                              vertical: 14.0, horizontal: 8)),
-                      shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
-                          (_) {
-                        return RoundedRectangleBorder(
-                          side: const BorderSide(
-                              color: Color(0xff1A1A1A), width: 1),
-                          borderRadius: BorderRadius.circular(12),
-                        );
-                      }),
-                    ),
-                  )
+                  PrimaryButton(
+                    buttonTitle: "already a member?",
+                    onPressed: () {},
+                    bgColor: Colors.white,
+                    textColor: Colors.black,
+                    border:
+                        const BorderSide(color: Color(0xff1A1A1A), width: 1),
+                  ),
                 ],
               ),
             ],
@@ -99,69 +87,127 @@ class CreateAccountOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-        ),
-      ),
-      padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 16, 0, 4),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Create an account",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => Navigator.of(context).pop(),
+      child: DraggableScrollableSheet(
+        initialChildSize: 0.9,
+        minChildSize: 0.65,
+        builder: (_, controller) => Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
             ),
           ),
-          const SizedBox(height: 32),
-          const InputField(
-            label: "Full Name",
-            placeholder: "Jhon Doe",
-            keyboard: TextInputType.name,
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: ListView(
+            controller: controller,
+            // mainAxisSize: MainAxisSize.min,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 16, 0, 4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Create an account",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 32),
+              const InputField(
+                label: "Full Name",
+                placeholder: "Jhon Doe",
+                keyboard: TextInputType.name,
+              ),
+              const InputField(
+                label: "E-mail",
+                placeholder: "someone@example.com",
+                keyboard: TextInputType.emailAddress,
+              ),
+              const InputField(
+                label: "Password",
+                placeholder: "•••••••••••••",
+                password: true,
+              ),
+              const SizedBox(height: 8),
+              PrimaryButton(
+                buttonTitle: "Continue",
+                onPressed: () {},
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Divider(
+                    color: Colors.grey,
+                    height: 1,
+                    thickness: 1,
+                  ),
+                  const Text("or"),
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xff00baff),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              PrimaryButton(
+                buttonTitle: "Continue with google",
+                onPressed: () {},
+                imageLeft: const Image(
+                  image: AssetImage('assets/google.png'),
+                  width: 26,
+                  height: 28,
+                ),
+                bgColor: const Color(0xff1A1A1A),
+              ),
+              const SizedBox(height: 8),
+              PrimaryButton(
+                buttonTitle: "Continue with facebook",
+                onPressed: () {},
+                bgColor: Colors.white,
+                imageLeft:
+                    const Image(image: AssetImage('assets/facebook.png')),
+                textColor: Colors.black,
+                border: const BorderSide(color: Color(0xff1A1A1A), width: 1),
+              ),
+              const SizedBox(height: 8),
+              PrimaryButton(
+                buttonTitle: "Already have an account? log in",
+                onPressed: () {},
+                bgColor: Colors.white,
+                textColor: const Color(0xffBD3A4A),
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                "by signing up, you agree to our privacy policy and T&C",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 11,
+                ),
+              ),
+              const SizedBox(height: 16),
+            ],
           ),
-          const InputField(
-            label: "E-mail",
-            placeholder: "someone@example.com",
-            keyboard: TextInputType.emailAddress,
-          ),
-          const InputField(
-            label: "Password",
-            placeholder: "•••••••••••••",
-            password: true,
-          ),
-          PrimaryButton(
-            buttonTitle: "Continue",
-            onPressed: () {},
-          ),
-          PrimaryButton(
-            buttonTitle: "Continue",
-            onPressed: () {},
-          ),
-          PrimaryButton(
-            buttonTitle: "Continue",
-            onPressed: () {},
-          )
-        ],
+        ),
       ),
     );
   }
