14764666cb54483add60d1e0b019b4bd8e218868
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-04-06 20:01:49 +0530

    create comm overlay 0.9v

--- lib/view/create_community_overlay.dart
+++ lib/view/create_community_overlay.dart
@@ -1,11 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:insync/widgets/button.dart';
 import 'package:insync/widgets/input_field.dart';
 
-class CreateCommunityOverlay extends StatelessWidget {
+class CreateCommunityOverlay extends StatefulWidget {
   const CreateCommunityOverlay({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<CreateCommunityOverlay> createState() => _CreateCommunityOverlayState();
+}
+
+class _CreateCommunityOverlayState extends State<CreateCommunityOverlay> {
+  bool isSwitched = false;
+  String visibilityVal = 'Private';
+  void toggleSwitch(bool value) {
+    if (isSwitched == false) {
+      setState(() {
+        isSwitched = true;
+        visibilityVal = 'Public';
+      });
+      print('Switch Button is ON');
+    } else {
+      setState(() {
+        isSwitched = false;
+        visibilityVal = 'Private';
+      });
+      print('Switch Button is OFF');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +38,7 @@ class CreateCommunityOverlay extends StatelessWidget {
       behavior: HitTestBehavior.opaque,
       onTap: () => Navigator.of(context).pop(),
       child: DraggableScrollableSheet(
-        initialChildSize: 0.6,
+        initialChildSize: 0.78,
         minChildSize: 0.4,
         builder: (_, controller) => Container(
           clipBehavior: Clip.hardEdge,
@@ -49,23 +74,65 @@ class CreateCommunityOverlay extends StatelessWidget {
                   ],
                 ),
               ),
-              const SizedBox(height: 32),
+              const SizedBox(height: 24),
+              PrimaryButton(
+                buttonTitle: "Upload community image",
+                iconLeft: const Icon(FeatherIcons.uploadCloud),
+                onPressed: () {},
+                textColor: const Color(0xff2561ED),
+                bgColor: Colors.white,
+                border: const BorderSide(color: Color(0xff2561ED), width: 2),
+              ),
+              const SizedBox(height: 8),
               const InputField(
                 label: "Community name",
                 placeholder: "Your community's name",
                 keyboard: TextInputType.name,
               ),
               const InputField(
-                label: "E-mail",
-                placeholder: "someone@example.com",
-                keyboard: TextInputType.emailAddress,
+                label: "Intrest topics",
+                keyboard: TextInputType.name,
+              ),
+              const InputField(
+                label: "Description",
+                placeholder: "About your community",
+                textArea: true,
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Visibility",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        visibilityVal,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  CupertinoSwitch(
+                    value: isSwitched,
+                    onChanged: toggleSwitch,
+                    activeColor: Colors.black,
+                  )
+                ],
               ),
               const SizedBox(height: 8),
               PrimaryButton(
                 buttonTitle: "Continue",
                 onPressed: () {},
               ),
-              const SizedBox(height: 8),
               const SizedBox(height: 16),
             ],
           ),
