8fec676798984275f60c24cc646d9d577c03bcc4
gitgudGR7 <sidzworld1@gmial.com>
2022-04-17 17:55:18 +0530

    added working interest selection page

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:insync/utils/theme_config.dart';
 import 'package:insync/view/community/discover_community.dart';
 import 'package:insync/view/forum/forum.dart';
 import 'package:insync/view/forum/forum_detail.dart';
+import 'package:insync/view/interest%20selection/selectinterest.dart';
 import 'package:insync/view/profile/aboutus.dart';
 import 'package:insync/view/profile/edit_profile.dart';
 import 'package:insync/view/splash.dart';
@@ -92,6 +93,8 @@ class _MyAppState extends State<MyApp> {
             '/discover': (context) => const DiscoverCommunity(),
             '/forum': (BuildContext context) => const ForumPage(),
             '/forum/1': (BuildContext context) => const ForumDetailPage(),
+            '/interests': (BuildContext context) => const Addinterests(),
+
           },
           title: 'InSync',
           theme: lightTheme,
@@ -128,6 +131,9 @@ class _InSyncState extends State<InSync> {
       Navigator.pushNamed(context, '/mainapp');
     } else {
       Navigator.pushNamed(context, '/authorization');
+      // Navigator.of(context).push(
+      //   MaterialPageRoute(builder: (context) => const Addinterests()),
+      // );
     }
   }
 
